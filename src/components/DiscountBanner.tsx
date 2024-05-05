f6b340ae61fb9460fae92836d8a2dcc2b3e07451
aezelion <abrahanqz@gmail.com>
2024-05-05 14:52:37 -0300

    Add User Props on members

--- src/components/DiscountBanner.tsx
+++ src/components/DiscountBanner.tsx
@@ -9,7 +9,7 @@ const DiscountBanner= () => {
             <div className="bg-[#ffffffab] min-w-[270px] sm:min-w-[300px] md:min-w-[500px] rounded-xl py-8 sm:px-8 px-2 grid place-items-center gap-3">
                 <button className="bg-blackish text-white p-2 rounded-md">25% DISCOUNT</button>
                 <h2 className="font-extrabold text-2xl text-[#272727]">Winter Sold</h2>
-                <p className="text-green-800 text-[20px]">Starting @ $20 <b>Shop Now</b></p>
+                <p className="text-green-800 text-[20px]">Starting $20 <b>Shop Now</b></p>
             </div>
           </div>
         </div>
--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Image from "next/image";
 
+interface UserProps {
+    img: string;
+    name: string;
+    role: string;
+    quote: string;
+  }
+
 const usersData = [
     {
         img: "/user.jpeg",
-        name: "Abrahan",
+        name: "Abrahan Quiroz",
         role: "CEO & Founder Invision",
         quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque similique totam iusto quas, quos, quae, quidem",
     },
     {
         img: "/user2.jpeg",
-        name: "Leonardo",
+        name: "Leonardo Diaz",
         role: "Web Design Consultant",
         quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque similique totam iusto quas, quos, quae, quidem",
     },
     // Agrega aquí más usuarios según sea necesario
 ];
 
-const User = ({ img, name, role, quote }) => {
+const User = ({ img, name, role, quote }: UserProps) => {
   return (
     <div className="border border-gray-300 rounded-2xl grid place-items-center p-6 lg:p-0">
       <div className="text-center flex flex-col items-center gap-1">
@@ -47,7 +54,7 @@ const Members = () => {
   return (
     <div>
       <div className="container pt-16">
-        <h2 className="font-medium text-2xl pb-4">Members</h2>
+        <h2 className="font-medium text-2xl pb-4"><b>Members</b></h2>
 
         <div className="mb-10 grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-15 ">
           {usersData.map((user, index) => (
